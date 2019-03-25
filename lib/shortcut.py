d8598e47c449bdb5530ca7c33c57b280ca57958d
Michael Wieland <git@selfcoders.com>
2019-03-25 19:00:20 +0100

    Do not wait for started processes to end before processing next shortcut

--- lib/shortcut.py
+++ lib/shortcut.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+from threading import Thread
 from typing import Dict
 
 import pyperclip
@@ -34,6 +35,66 @@ class Actions:
         for action in Actions.ACTIONS:
             if action.name == name:
                 return action
+
+
+class ExecThread(Thread):
+    def __init__(self, shortcut: "Shortcut"):
+        super().__init__()
+
+        self.shortcut = shortcut
+
+    def run(self):
+        if self.shortcut.action == Actions.LAUNCH_APPLICATION.name:
+            temp_desktop_file = os.path.join(os.path.expanduser("~"), ".local", "share", "applications", "keyboard-mapper-tmp.desktop")
+
+            try:
+                shutil.copy(self.shortcut.data, temp_desktop_file)
+            except Exception as exception:
+                self.show_execution_error(str(exception))
+                return
+
+            try:
+                subprocess.check_output(["gtk-launch", os.path.basename(temp_desktop_file)], stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as exception:
+                self.show_execution_error(exception.output)
+
+            os.remove(temp_desktop_file)
+        elif self.shortcut.action == Actions.EXECUTE_COMMAND.name:
+            try:
+                subprocess.check_output(self.shortcut.data, shell=True, stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as exception:
+                self.show_execution_error(exception.output)
+        elif self.shortcut.action == Actions.OPEN_FOLDER.name:
+            try:
+                subprocess.check_call(["xdg-open", self.shortcut.data], stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as exception:
+                self.show_execution_error(exception.output)
+        elif self.shortcut.action == Actions.INPUT_TEXT.name:
+            pyperclip.copy(self.shortcut.data)
+            XTestWrapper().send_combination(["Control_L", "V"])
+        elif self.shortcut.action == Actions.INPUT_KEY_SEQUENCE.name:
+            xtest_wrapper = XTestWrapper()
+
+            for combination in self.shortcut.data.split(" "):
+                xtest_wrapper.send_combination(combination.split("+"))
+        elif self.shortcut.action == Actions.LOCK_KEYS.name:
+            Shortcuts.instance.lock_keys.emit()
+
+    def show_execution_error(self, error_details: str):
+        message = [
+            translate("shortcut_error", "An error occurred while executing the action for key {} on device {}!").format(self.shortcut.key, self.shortcut.device),
+            "",
+            translate("shortcut_error", "Action: {}").format(self.shortcut.get_action_name())
+        ]
+
+        if error_details is not None and error_details != "":
+            if isinstance(error_details, bytes):
+                error_details = error_details.decode()
+
+            message.append("")
+            message.append(error_details)
+
+        Shortcuts.instance.execution_error.emit("\n".join(message))
 
 
 class Shortcut:
@@ -83,57 +144,12 @@ class Shortcut:
             return "{}: {}".format(Actions.get(self.action).title, self.data)
 
     def execute(self):
-        if self.action == Actions.LAUNCH_APPLICATION.name:
-            temp_desktop_file = os.path.join(os.path.expanduser("~"), ".local", "share", "applications", "keyboard-mapper-tmp.desktop")
+        thread = ExecThread(self)
 
-            try:
-                shutil.copy(self.data, temp_desktop_file)
-            except Exception as exception:
-                self.show_execution_error(str(exception))
-                return
+        # Do not wait for the thread once Keyboard Mapper termination has been requested, just stop it
+        thread.setDaemon(True)
 
-            try:
-                subprocess.check_output(["gtk-launch", os.path.basename(temp_desktop_file)], stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as exception:
-                self.show_execution_error(exception.output)
-
-            os.remove(temp_desktop_file)
-        elif self.action == Actions.EXECUTE_COMMAND.name:
-            try:
-                subprocess.check_output(self.data, shell=True, stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as exception:
-                self.show_execution_error(exception.output)
-        elif self.action == Actions.OPEN_FOLDER.name:
-            try:
-                subprocess.check_call(["xdg-open", self.data], stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as exception:
-                self.show_execution_error(exception.output)
-        elif self.action == Actions.INPUT_TEXT.name:
-            pyperclip.copy(self.data)
-            XTestWrapper().send_combination(["Control_L", "V"])
-        elif self.action == Actions.INPUT_KEY_SEQUENCE.name:
-            xtest_wrapper = XTestWrapper()
-
-            for combination in self.data.split(" "):
-                xtest_wrapper.send_combination(combination.split("+"))
-        elif self.action == Actions.LOCK_KEYS.name:
-            Shortcuts.instance.lock_keys.emit()
-
-    def show_execution_error(self, error_details: str):
-        message = [
-            translate("shortcut_error", "An error occurred while executing the action for key {} on device {}!").format(self.key, self.device),
-            "",
-            translate("shortcut_error", "Action: {}").format(self.get_action_name())
-        ]
-
-        if error_details is not None and error_details != "":
-            if isinstance(error_details, bytes):
-                error_details = error_details.decode()
-
-            message.append("")
-            message.append(error_details)
-
-        Shortcuts.instance.execution_error.emit("\n".join(message))
+        thread.start()
 
 
 class Shortcuts(QtCore.QObject):
