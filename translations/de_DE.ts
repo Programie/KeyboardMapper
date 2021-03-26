2c822633b1464a3316d60f8ff85bb1de84d46d0f
Michael Wieland <git@selfcoders.com>
2021-03-26 22:03:42 +0100

    Update lines in translation

--- translations/de_DE.ts
+++ translations/de_DE.ts
@@ -1,15 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de">
+<!DOCTYPE TS><TS version="1.1" language="de">
 <context>
     <name>about</name>
     <message>
-        <location filename="gui.py" line="1270"/>
+        <location filename="gui.py" line="1290"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1303"/>
+        <location filename="gui.py" line="1323"/>
         <source>View on GitLab</source>
         <translation>Auf GitLab zeigen</translation>
     </message>
@@ -17,122 +16,122 @@
 <context>
     <name>edit_shortcut</name>
     <message>
-        <location filename="gui.py" line="466"/>
+        <location filename="gui.py" line="486"/>
         <source>Edit shortcut</source>
         <translation>Shortcut bearbeiten</translation>
     </message>
     <message>
-        <location filename="gui.py" line="469"/>
+        <location filename="gui.py" line="489"/>
         <source>Add shortcut</source>
         <translation>Shortcut hinzufügen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="508"/>
+        <location filename="gui.py" line="528"/>
         <source>Shortcut</source>
         <translation>Shortcut</translation>
     </message>
     <message>
-        <location filename="gui.py" line="521"/>
+        <location filename="gui.py" line="541"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="gui.py" line="536"/>
+        <location filename="gui.py" line="556"/>
         <source>Action</source>
         <translation>Aktion</translation>
     </message>
     <message>
-        <location filename="gui.py" line="662"/>
+        <location filename="gui.py" line="682"/>
         <source>Browse...</source>
         <translation>Durchsuchen...</translation>
     </message>
     <message>
-        <location filename="gui.py" line="739"/>
+        <location filename="gui.py" line="759"/>
         <source>Click to set shortcut</source>
         <translation>Klicken um Shortcut festzulegen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="779"/>
+        <location filename="gui.py" line="799"/>
         <source>Select folder to open</source>
         <translation>Wähle den Ordner welcher geöffnet werden soll</translation>
     </message>
     <message>
-        <location filename="gui.py" line="802"/>
+        <location filename="gui.py" line="822"/>
         <source>No key defined</source>
         <translation>Keine Taste definiert</translation>
     </message>
     <message>
-        <location filename="gui.py" line="802"/>
+        <location filename="gui.py" line="822"/>
         <source>Please define a key to use for this shortcut!</source>
         <translation>Bitte definiere eine Taste welche für diesen Shortcut verwendet werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="808"/>
+        <location filename="gui.py" line="828"/>
         <source>Duplicate shortcut</source>
         <translation>Doppelter Shortcut</translation>
     </message>
     <message>
-        <location filename="gui.py" line="808"/>
+        <location filename="gui.py" line="828"/>
         <source>Another shortcut for key &apos;{}&apos; already exists!</source>
         <translation>Ein anderer Shortcut verwendet bereits die Taste &apos;{}&apos;!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="814"/>
+        <location filename="gui.py" line="834"/>
         <source>No action selected</source>
         <translation>Keine Aktion ausgewählt</translation>
     </message>
     <message>
-        <location filename="gui.py" line="814"/>
+        <location filename="gui.py" line="834"/>
         <source>Please select an action to do!</source>
         <translation>Bitte wähle eine Aktion welche ausgeführt werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="822"/>
+        <location filename="gui.py" line="842"/>
         <source>Missing application</source>
         <translation>Fehlende Anwendung</translation>
     </message>
     <message>
-        <location filename="gui.py" line="822"/>
+        <location filename="gui.py" line="842"/>
         <source>Please select the application to launch!</source>
         <translation>Bitte wähle die Anwendung welche gestartet werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="829"/>
+        <location filename="gui.py" line="849"/>
         <source>Missing command</source>
         <translation>Fehlender Befehl</translation>
     </message>
     <message>
-        <location filename="gui.py" line="829"/>
+        <location filename="gui.py" line="849"/>
         <source>Please specify the command to execute!</source>
         <translation>Bitte gebe den Befehl an welcher ausgeführt werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="836"/>
+        <location filename="gui.py" line="856"/>
         <source>Missing folder path</source>
         <translation>Fehlender Ordnerpfad</translation>
     </message>
     <message>
-        <location filename="gui.py" line="836"/>
+        <location filename="gui.py" line="856"/>
         <source>Please select the path to the folder to open!</source>
         <translation>Bitte wähle den Pfad zu dem Ordner aus welcher geöffnet werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="850"/>
+        <location filename="gui.py" line="870"/>
         <source>Missing text</source>
         <translation>Fehlender Text</translation>
     </message>
     <message>
-        <location filename="gui.py" line="843"/>
+        <location filename="gui.py" line="863"/>
         <source>Please specify the text to input!</source>
         <translation>Bitte gebe den Text an welcher eingegeben werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="850"/>
+        <location filename="gui.py" line="870"/>
         <source>Please specify the key sequence to input!</source>
         <translation>Bitte gebe die Tastenfolge an welche eingegeben werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="594"/>
+        <location filename="gui.py" line="614"/>
         <source>Edit...</source>
         <translation>Bearbeiten...</translation>
     </message>
@@ -142,57 +141,57 @@
         <translation type="obsolete">Beim Lesen der Desktop Dateien ist ein Fehler aufgetreten. Diese Dateien werden übersprungen.</translation>
     </message>
     <message>
-        <location filename="gui.py" line="564"/>
+        <location filename="gui.py" line="584"/>
         <source>Errors</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="gui.py" line="576"/>
+        <location filename="gui.py" line="596"/>
         <source>Loading desktop files failed</source>
         <translation>Laden der Desktop Dateien fehlgeschlagen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="562"/>
+        <location filename="gui.py" line="582"/>
         <source>An error occurred while parsing the desktop files.</source>
         <translation>Beim Lesen der Desktop Dateien ist ein Fehler aufgetreten.</translation>
     </message>
     <message>
-        <location filename="gui.py" line="574"/>
+        <location filename="gui.py" line="594"/>
         <source>Those files will be skipped.</source>
         <translation>Diese Dateien werden übersprungen.</translation>
     </message>
     <message>
-        <location filename="gui.py" line="635"/>
+        <location filename="gui.py" line="655"/>
         <source>Label options</source>
         <translation>Tastenbeschriftungsoptionen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="641"/>
+        <location filename="gui.py" line="661"/>
         <source>Icon</source>
         <translation>Symbol</translation>
     </message>
     <message>
-        <location filename="gui.py" line="646"/>
+        <location filename="gui.py" line="666"/>
         <source>Size</source>
         <translation>Größe</translation>
     </message>
     <message>
-        <location filename="gui.py" line="651"/>
+        <location filename="gui.py" line="671"/>
         <source>Background</source>
         <translation>Hintergrund</translation>
     </message>
     <message>
-        <location filename="gui.py" line="656"/>
+        <location filename="gui.py" line="676"/>
         <source>Preview</source>
         <translation>Vorschau</translation>
     </message>
     <message>
-        <location filename="gui.py" line="694"/>
+        <location filename="gui.py" line="714"/>
         <source>Click to change color</source>
         <translation>Klicken um die Farbe zu ändern</translation>
     </message>
     <message>
-        <location filename="gui.py" line="715"/>
+        <location filename="gui.py" line="735"/>
         <source>Open label icon</source>
         <translation>Tastenbeschriftungssymbol auswählen</translation>
     </message>
@@ -200,27 +199,27 @@
 <context>
     <name>key_sequence_builder</name>
     <message>
-        <location filename="gui.py" line="940"/>
+        <location filename="gui.py" line="960"/>
         <source>Build key sequence</source>
         <translation>Tastenfolge erstellen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="960"/>
+        <location filename="gui.py" line="980"/>
         <source>Add key combination to sequence</source>
         <translation>Tastenkombination zu Tastenfolge hinzufügen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="980"/>
+        <location filename="gui.py" line="1000"/>
         <source>Key combination</source>
         <translation>Tastenkombination</translation>
     </message>
     <message>
-        <location filename="gui.py" line="987"/>
+        <location filename="gui.py" line="1007"/>
         <source>Add key to combination</source>
         <translation>Taste zu Kombination hinzufügen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="992"/>
+        <location filename="gui.py" line="1012"/>
         <source>Remove this key combination</source>
         <translation>Diese Tastenkombination entfernen</translation>
     </message>
@@ -241,12 +240,12 @@
 <context>
     <name>main_window</name>
     <message>
-        <location filename="gui.py" line="113"/>
+        <location filename="gui.py" line="120"/>
         <source>Shortcuts locked, click to unlock</source>
         <translation>Shortcuts gesperrt, klicken zum Entsperren</translation>
     </message>
     <message>
-        <location filename="gui.py" line="367"/>
+        <location filename="gui.py" line="387"/>
         <source>Remove shortcut</source>
         <translation>Shortcut entfernen</translation>
     </message>
@@ -256,12 +255,12 @@
         <translation type="obsolete">Bist du dir sicher, dass du den Shortcut &apos;{}&apos; entfernen möchtest?</translation>
     </message>
     <message>
-        <location filename="gui.py" line="363"/>
+        <location filename="gui.py" line="383"/>
         <source>And {} more.</source>
         <translation>Und {} weitere.</translation>
     </message>
     <message>
-        <location filename="gui.py" line="367"/>
+        <location filename="gui.py" line="387"/>
         <source>Are you sure to remove the selected shortcuts?
 
 {}</source>
@@ -361,12 +360,12 @@
 <context>
     <name>main_window_statusbar</name>
     <message>
-        <location filename="gui.py" line="192"/>
+        <location filename="gui.py" line="204"/>
         <source>1 Shortcut</source>
         <translation>1 Shortcut</translation>
     </message>
     <message>
-        <location filename="gui.py" line="194"/>
+        <location filename="gui.py" line="206"/>
         <source>{} shortcuts</source>
         <translation>{} Shortcuts</translation>
     </message>
@@ -374,62 +373,62 @@
 <context>
     <name>settings</name>
     <message>
-        <location filename="gui.py" line="1065"/>
+        <location filename="gui.py" line="1085"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1083"/>
+        <location filename="gui.py" line="1103"/>
         <source>Enable tray icon</source>
         <translation>Tray Icon aktivieren</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1088"/>
+        <location filename="gui.py" line="1108"/>
         <source>Allow only one instance</source>
         <translation>Nur eine Instanz erlauben</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1092"/>
+        <location filename="gui.py" line="1112"/>
         <source>Start on login</source>
         <translation>Beim Anmelden starten</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1096"/>
+        <location filename="gui.py" line="1116"/>
         <source>Create desktop file</source>
         <translation>Desktop Datei erzeugen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1110"/>
+        <location filename="gui.py" line="1130"/>
         <source>Keyboard input device</source>
         <translation>Tastatureingabegerät</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1137"/>
+        <location filename="gui.py" line="1157"/>
         <source>Icon theme</source>
         <translation>Symbol Thema</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1241"/>
+        <location filename="gui.py" line="1261"/>
         <source>No keyboard input device selected</source>
         <translation>Kein Tastatureingabegerät ausgewählt</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1241"/>
+        <location filename="gui.py" line="1261"/>
         <source>Please select at least one input device to use!</source>
         <translation>Bitte wähle mindestens ein Eingabegerät aus welches verwendet werden soll!</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1151"/>
+        <location filename="gui.py" line="1171"/>
         <source>Labels</source>
         <translation>Tastenbeschriftungen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1170"/>
+        <location filename="gui.py" line="1190"/>
         <source>Default size</source>
         <translation>Standardgröße</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1194"/>
+        <location filename="gui.py" line="1214"/>
         <source>Icon margin</source>
         <translation>Symbolabstand</translation>
     </message>
@@ -449,7 +448,7 @@
         <translation>Pixel</translation>
     </message>
     <message>
-        <location filename="gui.py" line="1157"/>
+        <location filename="gui.py" line="1177"/>
         <source>Measure unit</source>
         <translation>Maßeinheit</translation>
     </message>
@@ -503,32 +502,32 @@
 <context>
     <name>shortcut_list_column</name>
     <message>
-        <location filename="gui.py" line="126"/>
+        <location filename="gui.py" line="133"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="gui.py" line="127"/>
+        <location filename="gui.py" line="134"/>
         <source>Action</source>
         <translation>Aktion</translation>
     </message>
     <message>
-        <location filename="gui.py" line="128"/>
+        <location filename="gui.py" line="135"/>
         <source>Key</source>
         <translation>Taste</translation>
     </message>
     <message>
-        <location filename="gui.py" line="129"/>
+        <location filename="gui.py" line="136"/>
         <source>Device</source>
         <translation>Gerät</translation>
     </message>
     <message>
-        <location filename="gui.py" line="130"/>
+        <location filename="gui.py" line="137"/>
         <source>Executions</source>
         <translation>Ausführungen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="131"/>
+        <location filename="gui.py" line="138"/>
         <source>Last execution</source>
         <translation>Letzte Ausführung</translation>
     </message>
@@ -536,17 +535,17 @@
 <context>
     <name>shortcut_requester</name>
     <message>
-        <location filename="gui.py" line="905"/>
+        <location filename="gui.py" line="925"/>
         <source>Configure key</source>
         <translation>Taste konfigurieren</translation>
     </message>
     <message>
-        <location filename="gui.py" line="911"/>
+        <location filename="gui.py" line="931"/>
         <source>Press the key to use.</source>
         <translation>Drücke die Taste welche verwendet werden soll.</translation>
     </message>
     <message>
-        <location filename="gui.py" line="913"/>
+        <location filename="gui.py" line="933"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
@@ -554,12 +553,12 @@
 <context>
     <name>tray_menu</name>
     <message>
-        <location filename="gui.py" line="179"/>
+        <location filename="gui.py" line="191"/>
         <source>Show window</source>
         <translation>Fenster zeigen</translation>
     </message>
     <message>
-        <location filename="gui.py" line="181"/>
+        <location filename="gui.py" line="193"/>
         <source>Quit</source>
         <translation>Beenden</translation>
     </message>
