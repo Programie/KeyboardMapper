cbfaac080b89ae2eb676c2341509f89e99536a38
Michael Wieland <git@selfcoders.com>
2023-04-29 20:41:26 +0200

    Bump version and year

--- lib/constants.py
+++ lib/constants.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 APP_NAME = "Keyboard Mapper"
 APP_DESCRIPTION = "A tool for Linux desktops to map keys of dedicated keyboards to specific actions."
-APP_COPYRIGHT = "© 2018-2022 Michael Wieland"
+APP_COPYRIGHT = "© 2018-2023 Michael Wieland"
 APP_WEBSITE = "https://gitlab.com/Programie/KeyboardMapper"
-APP_VERSION = "3.1"
+APP_VERSION = "3.3"
 BASE_DIR = Path(__file__).parent.parent
 DEVICES_BASE_DIR = Path("/dev/input/by-id")
 
