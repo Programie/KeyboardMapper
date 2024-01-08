d321176c8c5a9744712afab8078363553a06e212
Michael Wieland <git@selfcoders.com>
2024-01-08 14:39:21 +0100

    Fixed referencing instead of copying label of duplicated shortcut

--- lib/gui.py
+++ lib/gui.py
@@ -492,6 +492,7 @@ class EditShortcutWindow(QtWidgets.QDialog):
 
         if shortcut:
             self.shortcut = copy.copy(shortcut)
+            self.shortcut.label = copy.copy(self.shortcut.label)
             self.shortcut.executions = 0
             if duplicate:
                 self.setWindowTitle(translate("edit_shortcut", "Add shortcut"))
