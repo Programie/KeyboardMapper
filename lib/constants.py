6c2dd11bf1ccdf8f374eda8473e76664326f5509
Michael Wieland <git@selfcoders.com>
2024-01-08 15:46:58 +0100

    Update app version and copyright year

--- lib/constants.py
+++ lib/constants.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 APP_NAME = "Keyboard Mapper"
 APP_DESCRIPTION = "A tool for Linux desktops to map keys of dedicated keyboards to specific actions."
-APP_COPYRIGHT = "© 2018-2023 Michael Wieland"
+APP_COPYRIGHT = "© 2018-2024 Michael Wieland"
 APP_WEBSITE = "https://github.com/Programie/KeyboardMapper"
-APP_VERSION = "3.3"
+APP_VERSION = "3.4.0"
 BASE_DIR = Path(__file__).parent.parent
 DEVICES_BASE_DIR = Path("/dev/input/by-id")
 
